a8455064b61f234feb99b810f31a68f2e350b5eb
Raul <dobocan83@gmail.com>
2025-03-24 08:36:57 +0200

    Added Navigation layout

---
+++ Frontend/PawPal/app/(tabs)/chats.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+export default function Chats() {
+  return (
+    <View>
+      <Text>Chats</Text>
+    </View>
+  );
+}
